2294f820c4ce35c694babe0fde779a5bc7984738
Adityaraj1711 <adityamailbox0@gmail.com>
2020-04-17 02:55:34 +0530

    added user model for authentication

--- src/django_backend_api/api/models.py
+++ src/django_backend_api/api/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """
+    Represents a "user profile" inside out system. Stores all user account
+    related data, such as 'email address' and 'name'.
+    """
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """Django uses this when it needs to get the user's full name."""
+
+        return self.name
+
+    def get_short_name(self):
+        """Django uses this when it needs to get the users abbreviated name."""
+
+        return self.name
+
+    def __str__(self):
+        """Django uses this when it needs to convert the object to text."""
+
+        return self.email
+
+
+
+class UserProfileManager(BaseUserManager):
+    """Helps Django work with our custom user model."""
+
+    def create_user(self, email, name, password=None):
+        """Creates a new user profile."""
+
+        if not email:
+            raise ValueError('Users must have an email address.')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name,)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        """Creates and saves a new superuser with given details."""
+
+        user = self.create_user(email, name, password)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
