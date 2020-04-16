f34d1480f4da16e46564737949339e64950cf303
Adityaraj1711 <adityamailbox0@gmail.com>
2020-04-17 03:02:04 +0530

    added a custom user profile manager and migration to DB

--- src/django_backend_api/api/models.py
+++ src/django_backend_api/api/models.py
@@ -4,6 +4,35 @@ from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
 
 
+class UserProfileManager(BaseUserManager):
+    """Helps Django work with our custom user model."""
+
+    def create_user(self, email, name, password=None):
+        """Creates a new user profile."""
+
+        if not email:
+            raise ValueError('Users must have an email address.')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name,)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        """Creates and saves a new superuser with given details."""
+
+        user = self.create_user(email, name, password)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """
     Represents a "user profile" inside out system. Stores all user account
@@ -36,30 +65,3 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
         return self.email
 
 
-class UserProfileManager(BaseUserManager):
-    """Helps Django work with our custom user model."""
-
-    def create_user(self, email, name, password=None):
-        """Creates a new user profile."""
-
-        if not email:
-            raise ValueError('Users must have an email address.')
-
-        email = self.normalize_email(email)
-        user = self.model(email=email, name=name,)
-
-        user.set_password(password)
-        user.save(using=self._db)
-
-        return user
-
-    def create_superuser(self, email, name, password):
-        """Creates and saves a new superuser with given details."""
-
-        user = self.create_user(email, name, password)
-
-        user.is_superuser = True
-        user.is_staff = True
-        user.save(using=self._db)
-
-        return user
