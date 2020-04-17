c02d404f8c52c5a0307a295cf355b1d71b305329
Adityaraj1711 <adityamailbox0@gmail.com>
2020-04-17 15:52:33 +0530

    added model for storing profile feed items created and added to django admin

--- src/django_backend_api/api/models.py
+++ src/django_backend_api/api/models.py
@@ -70,3 +70,17 @@ class Message(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class ProfileFeedItem(models.Model):
+    """Profile status update."""
+
+    user_profile = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return the model as a string."""
+
+        return self.status_text
+
