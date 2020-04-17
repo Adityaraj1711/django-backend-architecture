a22b373e288fca08cf1f9b4367201f68fa70f3f7
Adityaraj1711 <adityamailbox0@gmail.com>
2020-04-17 15:14:44 +0530

    added search function to user profile api

--- src/django_backend_api/api/views.py
+++ src/django_backend_api/api/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 from . import serializers
 from . import models
 from . import permission
@@ -101,3 +102,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permission.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email')
