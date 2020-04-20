d0d0dc12aeba895ec09b53139839550d0707ce2d
Adityaraj1711 <adityamailbox0@gmail.com>
2020-04-21 02:56:41 +0530

    changed database from SQL to postgresql

--- src/django_backend_api/django_backend_api/settings.py
+++ src/django_backend_api/django_backend_api/settings.py
@@ -76,11 +76,22 @@ WSGI_APPLICATION = 'django_backend_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default':
+        {'ENGINE': 'django.db.backends.postgresql_psycopg2',
+                  'NAME': 'mysite',
+                  'USER': 'mysite',
+                  'PASSWORD': 'mysite',
+                  'HOST': 'localhost',
+                  'PORT': '',
+        }
 }
 
 
