7fe976eaa1f353e3b8bdc9cbd16c5bfef7f0714f
Adityaraj1711 <adityamailbox0@gmail.com>
2020-04-21 03:03:48 +0530

    make media url available in development server

--- src/django_backend_api/django_backend_api/urls.py
+++ src/django_backend_api/django_backend_api/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.conf.urls import url
 from django.conf.urls import include
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include('api.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
