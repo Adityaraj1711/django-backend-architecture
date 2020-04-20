c3663e1ff6ca8529cad7cded27475671081865c5
Adityaraj1711 <adityamailbox0@gmail.com>
2020-04-21 02:17:22 +0530

    added tests for project model

--- src/django_backend_api/api/tests/test_project_api.py
+++ src/django_backend_api/api/tests/test_project_api.py
@@ -6,32 +6,32 @@ from rest_framework import status
 from rest_framework.test import APIClient
 from rest_framework import permissions
 
-from api.models import College
-from api.serializers import CollegeSerializer
+from api.models import Project
+from api.serializers import ProjectSerializer
 
 
-COLLEGE_URL = reverse('college-list')
+PROJECT_URL = reverse('project-list')
+
 
 def create_user(**params):
     """Helper function to create new user"""
     return get_user_model().objects.create_user(**params)
 
 
-class PublicCollegeApiTests(TestCase):
-    """Test unauthenticated college get API access"""
+class PublicProjectApiTests(TestCase):
+    """Test unauthenticated projects get API access"""
 
     def setUp(self):
         self.client = APIClient()
 
     def test_required_auth(self):
         """Test the authenticaiton is required"""
-        res = self.client.get(COLLEGE_URL)
-
+        res = self.client.get(PROJECT_URL)
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
-class PrivateCollegeApiTests(TestCase):
-    """Test the authorized user college API"""
+class PrivateProjectApiTests(TestCase):
+    """Test the authorized user project API"""
 
     def setUp(self):
         email = 'emailtest@developer.com'
@@ -46,19 +46,18 @@ class PrivateCollegeApiTests(TestCase):
         self.client.force_authenticate(self.user)
 
     def test_retrieve_tags(self):
-        """Test retrieving college objects"""
-        College.objects.create(college_name="user4",
-                               college_address="user4",
-                               grade="user4",
-                               degree="user4",
-                               from_date="2016-04-20T19:49:25.541768Z",
-                               to_date="2020-04-20T19:49:25.541799Z",
+        """Test retrieving project objects"""
+        Project.objects.create(project="mdtouch",
+                               about="centralized website",
+                               feature="web and desktop apps",
+                               tech_stack="django, python, angular, sql, android, pyQt5",
+                               project_url="http://127.0.0.1:8080/api/projects/6",
                                user_profile=self.user
         )
 
-        res = self.client.get(COLLEGE_URL)
-        college = College.objects.all().order_by('-college_name')
-        serializer = CollegeSerializer(college, many=True)
+        res = self.client.get(PROJECT_URL)
+        project = Project.objects.all() 
+        serializer = ProjectSerializer(project, many=True)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
 
